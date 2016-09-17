6c24b7e9e5dd5a8d7c031c146c5ab05142c6963c
Hamurabi Araújo <araujohamurabi@gmail.com>
2016-09-17 09:07:08 -0300

    Função IP iniciada

--- SDES.java
+++ SDES.java
@@ -1,10 +1,18 @@
 package br.ufrn.imd.scripts;
 
 public class SDES {
-
+	public Character IP (Character c, int[] table) {
+		System.out.println(Integer.toBinaryString(c));
+		
+		for (int i = 0; i < table.length; i++) {
+			
+		}
+		return c;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SDES sdes = new SDES();
+		
 	}
 
 }
